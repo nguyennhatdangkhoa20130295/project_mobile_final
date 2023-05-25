53da73014901aab21a39fe6bdb42a7a0fe9f96a6
DangKhoa <20130295@st.hcmuaf.edu.vn>
2023-05-25 22:27:29 +0700

    first commit

---
+++ app/src/main/java/com/example/mobile_project_food_order_application/Common/Common.java
@@ -0,0 +1,8 @@
+package com.example.mobile_project_food_order_application.Common;
+
+import com.example.mobile_project_food_order_application.Model.User;
+
+public class Common {
+    public static User currentUser;
+    public static final String DELETE = "Delete";
+}
